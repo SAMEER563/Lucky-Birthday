a0aabb4e4d07023c56afb43c539bb7de5a3b0417
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2023-07-24 10:29:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+
+
+function compareValues(sum, luckyNumber) {
+    if(sum%luckyNumber === 0) {
+        outputBox.innerText ="Your birthday is lucky🎉";
+    } else {
+        outputBox.innerText ="Your birthday is not lucky😒";
+    }
+}
+
+
+function checkBirthDateIsLucky(){
+    const dob=dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if (sum&&dob)
+    compareValues(sum, luckyNumber.value)
+    else
+    outputBox.innerText ="Please enter both the fields🫠";
+}
+
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", "");
+  let sum =0; 
+  for(let index=0; index<dob.length; index++) {
+    sum = sum + Number(dob.charAt(index));
+  }
+  return sum;
+}
+
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky)
